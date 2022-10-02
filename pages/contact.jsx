bd6abb0118677904f1d10088b295a93af2766756
Your Name <you@example.com>
2022-10-02 18:36:58 -0300

    primer commit

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const contact = () => {
+	return (
+		<Layout title='Contact | next.js' description='agregue una descripcion'>
+			<h1>contact</h1>
+		</Layout>
+	);
+};
+
+export default contact;
